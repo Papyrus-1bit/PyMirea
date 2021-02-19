7415f1ae72d0bc6790e339aa6776c69a7a6df7e0
vladimir <petr60206@gmail.com>
2021-02-19 11:05:10 +0300

    add lab2 2-1

--- 2lab/v23(2_1).py
+++ 2lab/v23(2_1).py
@@ -10,14 +10,15 @@ def f(arr:list):
             if arr[0]=='hack':
                 return 0
             elif arr[0]=='tcsh':
-                if arr[3]==1959:
+                if arr[4]==1959:
                     return 2
-                elif arr[3]==1997:
+                elif arr[4]==1997:
                     return 1
 
 def main():
     print(f(['hack', 1991, 1980, 'less', 1959]))
     print(f(['hack', 2012, 1966, 'less', 1959]))
+    print(f(['tcsh', 2006, 1966, 'less', 1997]))
 
 if __name__ == '__main__':
     main()
